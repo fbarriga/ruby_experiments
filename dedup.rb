9c7de0d72c7bf074ffbabfad8d16b4a8e6649363
Roman Gaufman <hackeron@gmail.com>
2011-07-11 04:22:55 +0100

    Sanity Check

--- dedup.rb
+++ dedup.rb
@@ -10,7 +10,8 @@ include FileUtils
 TEST_FRAMES = 50
 START_TIME = "00:01:00"
 #SEARCH_DIR = File.expand_path("/Volumes/Store/duptest")
-SEARCH_DIR = File.expand_path("/Volumes/Storage/Downloads/asd")
+#SEARCH_DIR = File.expand_path("/Volumes/Storage/Downloads/asd")
+SEARCH_DIR = File.expand_path("/Volumes/Data/Backups/asd")
 TMP_DIR = File.expand_path("/Volumes/Store/tmp")
 
 # Create directories if they don't exist
@@ -31,19 +32,29 @@ dup_hash = {}
 
 Dir.chdir(TMP_DIR) do
   Find.find(SEARCH_DIR) do |filename|
+    
     # Skip if not a file or not video
     next if ! File.file?(filename)
     filemime = MimeMagic.by_path(filename)
     next if ! filemime || ! filemime.video?
     puts "*** Analysing: %s" % filename
+    
     # Get some frames with mplayer
     print "\tMplayer "
     quietrun(['mplayer', '-nosound', '-vo', 'jpeg', '-ss', START_TIME, 
               '-frames', TEST_FRAMES.to_s, filename])
+       
+    # Sanity check       
+    if %x(ls *.jpg).split("\n").length < 1
+      puts "\n\tUnable to create images, skipping..."
+      next
+    end
+
     # Create 10x10 black/white threshold images
     print "|| ImageMagick "
     quietrun(['mogrify', '-resize', '16x16!', '-threshold', '50%',
               '-format', 'bmp', '*.jpg'])
+              
     # Create MD5Sum for each image file
     print "|| MD5Sums\n"
     %x(ls *.bmp | sort -n).split("\n").each do |tmpimg|
@@ -57,13 +68,15 @@ Dir.chdir(TMP_DIR) do
         dup_hash[key] += 1
       end
     end
+    
     # Delete the images created by mogrify and mplayer
     rm(%x(ls *.bmp *.jpg).split("\n"))
+    
   end
 end
 
 # Print duplicates
 dup_hash.sort{|a,b| a[1] <=> b[1]}.each do |a,b|
-  puts "--- Potential Duplicate: matching frames: %s%%" % (100*b/TEST_FRAMES)
+  puts "--- Potential Duplicate: matching frames: %.02f%%" % (100*b/TEST_FRAMES)
   a.each { |filename| puts "\t%s" % filename }
 end
